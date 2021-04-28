98059acc5ad068ff9d2f9d5a46905d96a06c7348
aakashkumar <aakashthesid@gmail.com>
2021-04-28 23:15:57 +0300

    Initial commit

---
+++ src/main/java/com/prep/prepproject/PrepProjectApplication.java
@@ -0,0 +1,13 @@
+package com.prep.prepproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prep/prepproject/PrepProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prep.prepproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
