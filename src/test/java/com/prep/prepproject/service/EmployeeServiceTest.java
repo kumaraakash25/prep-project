bdaeabee5cee7bfa3cc5ac83ee8947ee27daedbe
aakashkumar <aakashthesid@gmail.com>
2021-05-09 20:09:17 +0300

    Adding tests

---
+++ src/test/java/com/prep/prepproject/service/EmployeeServiceTest.java
@@ -0,0 +1,18 @@
+package com.prep.prepproject.service;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeeServiceTest {
+
+    @Test
+    void getEmployee() {
+    }
+
+    @Test
+    void updateEmployee() {
+    }
+
+    @Test
+    void createEmployee() {
+    }
+}
